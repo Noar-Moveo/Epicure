f2c148cd8f7a57e04dc26b461c68a63afc28ddcd
Noa Reuven <noar@moveo.co.il>
2024-04-25 11:42:57 +0300

    fix

--- Epicure/src/components/Hero/Hero.tsx
+++ Epicure/src/components/Hero/Hero.tsx
@@ -2,22 +2,16 @@ import NevigationBar from "../NavigationBar/NavigationBar.tsx";
 import HeaderImage from "../../assets/Images/Header.png";
 import { HeaderContainer, HeaderImg } from "./HeroStyles.ts";
 import SearchBar from "../SearchBar/SearchBar.tsx";
-import UserIcon from "../../assets/Icons/Person.png";
-import MenuIcon from "../../assets/Icons/Menu.png";
-import ExploreIcon from "../../assets/Icons/Explore.png";
-import OrderIcon from "../../assets/Icons/Order.png";
 import { placeholder_searchBar } from "../../data/resources.ts";
 import { menuOptions, menuItems } from "../../data/Menu.ts";
+import { icons } from "../../data/Menu.ts";
 
 function Hero() {
-  const RESOURCES = {
-    ICONS: [MenuIcon, ExploreIcon, UserIcon, OrderIcon],
-  };
 
   return (
     <>
       <NevigationBar
-        icons={RESOURCES.ICONS}
+        icons={icons}
         menuOptions={menuOptions}
         menuItems={menuItems}
       />
@@ -25,7 +19,7 @@ function Hero() {
         <HeaderImg src={HeaderImage} alt="Header" />
         <SearchBar
           placeholder={placeholder_searchBar}
-          icon={RESOURCES.ICONS[1]}
+          icon={icons[1]}
         />
       </HeaderContainer>
     </>
